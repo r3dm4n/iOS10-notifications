0561664822c373c8e17386229420ce976be2a006
Alexandru Corut <r3dm4n@me.com>
2017-05-10 07:51:58 +0300

    basic text notification

--- iOS10-notifications/ViewController.swift
+++ iOS10-notifications/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //1. REQUEST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func notifyMeButtonTapped(sender: UIButton) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
     }
 
+// 2. CREATE NOTIFICATION
+func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+    let notif = UNMutableNotificationContent()
+    notif.title = "New Notification"
+    notif.subtitle = "These are great"
+    notif.body = "The new notification options in iOS10 are great!"
 
+    let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+
+    let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+
+    UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+        if error != nil {
+            print(error)
+            completion(false)
+        } else {
+            completion(true)
+        }
+    })
+
+}
 }
 
